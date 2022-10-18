3bfe91668b81fc7d9903135543e61194faa68283
Simon Eich <69088336+SimonEich@users.noreply.github.com>
2022-10-18 17:05:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 document.getElementById("Date").addEventListener("click",dat);
 document.getElementById("Time").addEventListener("click",tim);
 
-const d = new Date();
+var state = "Start";
+var wert = "startwert";
 
-function tim(){
-    document.getElementById("Zeit").innerHTML = d.toLocaleTimeString();
-    document.getElementById("h1").innerHTML = "Time";
-
-
-
+function tim() {
+  state = "Time"
 }
+
+
+
 
 function dat() {
-  document.getElementById("Zeit").innerHTML = d.getDay()+"."+d.getMonth()+"." + d.getFullYear();
-  document.getElementById("h1").innerHTML = "Date";
-  return;
+  state = "Datum";
+  wert = "1.1.1"
+  show();
 }
+
+
+
+
+const myInterval = setInterval(show, 1000);
+
+function show(){
+
+  switch (state){
+    case "Start":
+    wert = "Ready to start!";
+    break;
+
+    case "Time":
+
+    console.log(state);
+    function myTimer() {
+      const d = new Date();
+      time = d.toLocaleTimeString();
+      return time;
+    }
+    wert = myTimer();
+    break;
+
+    case "Datum":
+      const d = new Date();
+      wert = d.getDate()+"."+ (d.getMonth() + 1) +"." + d.getFullYear();
+    break;
+
+
+  }
+
+  document.getElementById("h1").innerHTML = state;
+  document.getElementById("Zeit").innerHTML = wert;
+  }
